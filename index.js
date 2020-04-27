8712ef8c0e4ec8f66ecb3bedf4ba7f450c4fcdd1
isaac-angelo <isaac.angelo@hotmail.com>
2020-04-27 19:22:22 -0300

    ate o video 32

---
+++ index.js
@@ -0,0 +1,49 @@
+var fields = document.querySelectorAll("#form-user-create [name]");
+
+var user = {};
+
+fields.forEach(function(field, index){
+
+    if (field.name == "gender") {
+
+        if (field.checked) {
+
+            user[field.name] = field.value;
+
+        }
+
+    } else {
+
+        user[field.name] = field.value;
+
+    }
+
+});
+
+document.getElementById("form-user-create").addEventListener("submit", function(){
+
+    event.preventDefault();
+
+    var user = {};
+
+    fields.forEach(function(field, index){
+
+        if (field.name == "gender") {
+
+            if (field.checked) {
+
+                user[field.name] = field.value;
+
+            }
+
+        } else {
+
+            user[field.name] = field.value;
+
+        }
+    
+    });
+
+    console.log(user);
+
+});
